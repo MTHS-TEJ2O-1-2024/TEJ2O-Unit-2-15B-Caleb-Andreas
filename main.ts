4f1f37246daf230e6fb99e56f94b9634cc3eb084
Caleb-Andreas <caleb.andreas@mths.ca>
2024-11-05 11:23:52 -0500

    started code

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Caleb Andreas
+ * Created on: Nov 2024
+ * This program is a turtle logo program.
 */
 
 basic.showString('Hello, World!')
